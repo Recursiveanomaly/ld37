63fd6fd48cd12d7741ded730784b9bbeca49296e
augusthippo <david.law.hein@gmail.com>
2016-12-10 16:48:01 -0700

    adding GrowthController.cs and test_map..csv data

---
+++ UnityProjects/ld37/Assets/GrowthController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowthController : MonoBehaviour
+{
+    public List<TextAsset> growthPhaseOne = new List<TextAsset>();
+    public List<TextAsset> growthPhaseTwo = new List<TextAsset>();
+    public List<TextAsset> growthPhaseThree = new List<TextAsset>();
+}
