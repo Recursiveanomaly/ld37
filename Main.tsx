30fac17b748499f4a5b5f389bde09ff5eac181bd
DrDeleto <cpfloyd@comcast.net>
2016-12-11 01:23:13 -0700

    added tileset for Tiled

---
+++ Main.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Main" tilewidth="128" tileheight="128" tilecount="2" columns="0">
+ <tile id="0">
+  <image width="128" height="128" source="UnityProjects/ld37/Assets/Sprites/Environment/tile.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="UnityProjects/ld37/Assets/Sprites/Environment/wooden-door.png"/>
+ </tile>
+</tileset>
